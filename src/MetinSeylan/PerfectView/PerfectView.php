0f8b5419e482f3c4d6d6d2a58c1735b10dad8c20
Metin Seylan <metinsyln@gmail.com>
2013-12-06 11:46:37 +0200

    ajax belirtilmemiş değişken fix

--- src/MetinSeylan/PerfectView/PerfectView.php
+++ src/MetinSeylan/PerfectView/PerfectView.php
@@ -78,7 +78,7 @@ class PerfectView {
 
     public function make($view, $data = array(), $option = array('ajax' => true, 'onlyContent' => false, 'jsonData' => false, 'nonBase' => false)) {
         
-        if(Request::ajax() AND $option['ajax']){
+        if(Request::ajax() AND @$option['ajax']){
             return $this->makeJson($view, $data, @$option['jsonData']);
         }
         
